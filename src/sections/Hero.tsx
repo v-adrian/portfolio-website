4752300eeda8892cdd71b18c4025717041f7d868
v-adrian <vonnadrianjutar123@gmail.com>
2026-07-10 12:07:39 +0800

    Finish hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,26 @@
+import { TypeAnimation } from "react-type-animation";
+
 function Hero() {
   return (
-    <></>
-  )
+    <section className="mx-4">
+      <div className="mt-16 text-2xl">
+        Hello, I'm <br />
+        <span className="text-6xl font-bold">Vonn Adrian Jutar</span>
+      </div>
+      <TypeAnimation
+        sequence={[
+          "Software Developer",
+          1000,
+          "Software Engineer",
+          1000,
+          "Full-Stack Developer",
+          1000,
+        ]}
+        repeat={Infinity}
+        className="text-4xl font-semibold"
+      />
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
