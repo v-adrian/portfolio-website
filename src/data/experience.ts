a889446e9746a137694879ced73b26e74a1b3a46
v-adrian <vonnadrianjutar123@gmail.com>
2026-07-14 00:55:28 +0800

    Placeholder text

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -8,7 +8,7 @@ export const experience: Experience[] = [
     location: "Davao City, Philippines",
     duration: "January 2026 — Present",
     description: [
-      "",
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?",
     ],
   },
   {
