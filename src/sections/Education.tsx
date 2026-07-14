fb3dd57f832cd6bdce63638098b991b8ddbfaf1d
v-adrian <vonnadrianjutar123@gmail.com>
2026-07-14 10:56:34 +0800

    Finish education section

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -1,7 +1,27 @@
+import { education } from "@/data/education";
+
 function Education() {
   return (
-    <></>
-  )
+    <section
+      id="education"
+      className="grid grid-cols-1 gap-8 sm:grid-cols-[12rem_1fr]"
+    >
+      <h2 className="font-heading text-lg font-bold">Education</h2>
+      <div className="space-y-8 divide-y">
+        {education.map((item) => (
+          <div key={item.id} className="space-y-4 pb-8">
+            <div className="flex justify-between">
+              <div>
+                <h3 className="text-lg font-semibold">{item.degree}</h3>
+                <p className="text-md italic">{item.institution}</p>
+              </div>
+              <p className="text-sm">{item.duration}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Education
+export default Education;
