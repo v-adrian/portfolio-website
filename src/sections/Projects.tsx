d918eda28fca1b0cb32afaa48e1605eb035c035b
v-adrian <vonnadrianjutar123@gmail.com>
2026-07-14 13:29:00 +0800

    Finish projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,7 +1,20 @@
+import { projects } from "@/data/projects";
+import Card from "@/components/Card";
+
 function Projects() {
   return (
-    <></>
-  )
+    <section
+      id="projects"
+      className="grid grid-cols-1 gap-8 sm:grid-cols-[12rem_1fr]"
+    >
+      <h2 className="font-heading text-lg font-bold">Projects</h2>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {projects.map((item) => {
+          return <Card project={item}></Card>;
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
